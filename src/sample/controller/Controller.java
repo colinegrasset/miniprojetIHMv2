f10ecaf21ceaf89139ecca79f6cc2d5306dad2d4
coline <coline-grasset@outlook.fr>
2021-04-27 15:48:38 +0200

    new file

---
+++ src/sample/controller/Controller.java
@@ -0,0 +1,11 @@
+package sample.controller;
+
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML private int test;
+
+    public Controller(int test) {
+        this.test = test;
+    }
+}
