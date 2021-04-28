6180556fa3bfe26c9c089df3006d55f96573ba5f
MelodieGA <74061657+MelodieGA@users.noreply.github.com>
2021-04-28 16:05:05 +0200

    création classe Etudiant

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,30 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import sample.model.Etudiant;
 
 public class Main extends Application {
+
+    private ObservableList<Etudiant> donneesEtudiant = FXCollections.observableArrayList();
+
+    public Main() {
+        // Ajout de quelques données
+        donneesEtudiant.add(new Etudiant("VIDAL", "Océane", "2000", "L3", null));
+        donneesEtudiant.add(new Etudiant("BOUHIER", "Noé", "1999", "L3", null));
+        donneesEtudiant.add(new Etudiant("LECLERE", "Amandine", "1999", "M1", "physio"));
+    }
+
+    public ObservableList<Etudiant> getDonneesEtudiant() {
+        return donneesEtudiant;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
---
+++ src/sample/model/Etudiant.java
@@ -0,0 +1,96 @@
+package sample.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Etudiant {
+
+    private StringProperty nom;
+    private StringProperty prenom;
+    private StringProperty anneeNaissance;
+    private StringProperty promotion;
+    private StringProperty option;
+
+
+    /**
+     * Constructor with some initial data.
+     *
+     * @param nom
+     * @param prenom
+     * @param anneeNaissance
+     * @param promotion
+     * @param option
+     */
+    public Etudiant(String nom, String prenom, String anneeNaissance, String promotion, String option) {
+        this.nom = new SimpleStringProperty(nom);
+        this.prenom = new SimpleStringProperty(prenom);
+        this.anneeNaissance = new SimpleStringProperty(anneeNaissance);
+        this.promotion = new SimpleStringProperty(promotion);
+        this.option = new SimpleStringProperty(option);
+    }
+
+    public String getNom() {
+        return nom.get();
+    }
+
+    public StringProperty nomProperty() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom.set(nom);
+    }
+
+    public String getPrenom() {
+        return prenom.get();
+    }
+
+    public StringProperty prenomProperty() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom.set(prenom);
+    }
+
+    public String getAnneeNaissance() {
+        return anneeNaissance.get();
+    }
+
+    public StringProperty anneeNaissanceProperty() {
+        return anneeNaissance;
+    }
+
+    public void setAnneeNaissance(String anneeNaissance) {
+        this.anneeNaissance.set(anneeNaissance);
+    }
+
+    public String getPromotion() {
+        return promotion.get();
+    }
+
+    public StringProperty promotionProperty() {
+        return promotion;
+    }
+
+    public void setPromotion(String promotion) {
+        this.promotion.set(promotion);
+    }
+
+    public String getOption() {
+        return option.get();
+    }
+
+    public StringProperty optionProperty() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option.set(option);
+    }
+}
+
